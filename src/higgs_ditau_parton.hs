793243ce642aa31c83957c672bcafff56598e6de
cbpark <cbpark@gmail.com>
2015-09-11 17:03:27 +0900

    Add basic cuts

--- src/higgs_ditau_parton.hs
+++ src/higgs_ditau_parton.hs
@@ -21,14 +21,21 @@ main = do
 
   infile <- head <$> getArgs
   withFile infile ReadMode $ \hin ->
-    runEffect $ U.eventEntry hin >-> U.finalStates >-> U.groupByMother
-    >-> P.map (variables . mconcat . map part)
+    runEffect $ U.eventEntry hin >-> U.finalStates >-> basicSelection
+    >-> U.groupByMother >-> P.map (variables . mconcat . map part)
     >-> P.print
+
+basicSelection :: Monad m => Pipe [Particle] [Particle] m ()
+basicSelection = P.map (filter ((||) <$> ((`elem` neutrinos) . idOf)
+                                <*> ((&&) <$> ((>0.5) . pt) <*> ((<2.5) . abs . eta))))
 
 part :: [Particle] -> KinematicObjects
 part ps =
   let (invis, vis) = partition ((`elem` neutrinos) . idOf) ps
-  in KinematicObjects ((transverseVector . momentumSum) invis) [momentumSum vis]
+      visSum = momentumSum vis
+      visSelected | pt visSum > 20.0 = [visSum]
+                  | otherwise        = []
+  in KinematicObjects ((transverseVector . momentumSum) invis) visSelected
 
 data KinematicObjects = KinematicObjects { missing :: TransverseMomentum
                                          , visible :: [FourMomentum]
@@ -45,22 +52,26 @@ instance Show Result where
   show = intercalate "," . map (show . snd) . getResult
 
 variables :: KinematicObjects -> Result
-variables KinematicObjects { .. } =
-  let mTtrue = transverseMassCluster visible missing
-      mVisible = invariantMass visible
-      mEffective = invariantMass (fourMomentum missing : visible)
-      twoVisibles = if length visible /= 2 then [] else visible
-      (mT2, mTHiggsBound)
-        | null twoVisibles = (-1, -1)
-        | otherwise        = let (visA:(visB:_)) = twoVisibles
-                             in ( mT2func visA visB missing 0
-                                , mTBound visA visB missing mTau )
-  in Result [ ("mTtrue",       mTtrue       )
-            , ("mVisible",     mVisible     )
-            , ("mEffective",   mEffective   )
-            , ("mT2",          mT2          )
-            , ("mTHiggsBound", mTHiggsBound )
-            ]
+variables KinematicObjects { .. }
+  | pt missing > 20.0 && length visible == 2 =
+      let mTtrue = transverseMassCluster visible missing
+          mVisible = invariantMass visible
+          mEffective = invariantMass (fourMomentum missing : visible)
+          (mT2, mTHiggsBound) = let (visA:(visB:_)) = visible
+                                in ( mT2func visA visB missing 0
+                                   , mTBound visA visB missing mTau )
+      in Result [ ("mTtrue",       mTtrue       )
+                , ("mVisible",     mVisible     )
+                , ("mEffective",   mEffective   )
+                , ("mT2",          mT2          )
+                , ("mTHiggsBound", mTHiggsBound )
+                ]
+  | otherwise = Result [ ("mTtrue",       0)
+                       , ("mVisible",     0)
+                       , ("mEffective",   0)
+                       , ("mT2",          0)
+                       , ("mTHiggsBound", 0)
+                       ]
 
 mT2func :: FourMomentum -> FourMomentum -> TransverseMomentum -> Double -> Double
 mT2func visA visB ptmiss mInv = case mT2SymmMinuit2 visA visB ptmiss mInv of
